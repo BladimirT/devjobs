205e905399a98c3109ed7a2c510ad409058781d9
BladimirT <bladimirtetzaguic@gmail.com>
2023-12-26 15:53:05 -0600

    Eliminar vacantes con sweetalert y livewire

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -18,8 +18,8 @@
                 <a href="{{ route('vacantes.edit', $vacante->id) }}"
                     class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Editar</a>
 
-                <a href="#"
-                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Eliminar</a>
+                <button wire:click="$dispatch('mostrarAlerta', {{ $vacante->id }})"
+                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Eliminar</button>
             </div>
         </div>
 
@@ -32,3 +32,34 @@
     </div>
 </div>
 
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        document.addEventListener('livewire:initialized', () => {
+            @this.on('mostrarAlerta', (vacanteId) => {
+                Swal.fire({
+                    title: '¿Eliminar Vacante?',
+                    text: "Una Vacante eliminada no se puede recuperar:(",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Si, eliminar!',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // ELiminar vacante
+                        @this.call('eliminarVacante', vacanteId);
+                        Swal.fire(
+                            'Se eliminó la Vacante',
+                            'Eliminado correctamente',
+                            'success'
+                        )
+                    }
+                })
+
+            });
+        });
+    </script>
+@endpush
